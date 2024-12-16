2ff7d755512fab5427243be603111f751e573d3d
WireWhiz <eli@knisley.info>
2024-12-16 01:11:59 -0500

    update repo config to match custom url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,121 +1,121 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'BraneScript Docs',
-  tagline: 'Threads are what tie the universe together',
-  favicon: 'img/favicon.ico',
+    title: 'BraneScript Docs',
+    tagline: 'Threads are what tie the universe together',
+    favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://wirewhiz.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/BraneScript/',
+    // Set the production url of your site here
+    url: 'https://scripting.thebrane.org',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'WireWhiz', // Usually your GitHub org/user name.
-  projectName: 'BraneScript', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'WireWhiz', // Usually your GitHub org/user name.
+    projectName: 'BraneScript', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  markdown: {
-    mermaid: true,
-  },
+    markdown: {
+        mermaid: true,
+    },
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/WireWhiz/BraneScript',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/WireWhiz/BraneScript',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
-  themes: ['@docusaurus/theme-mermaid'],
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/branebrain.png',
-    navbar: {
-      title: 'BraneScript',
-      logo: {
-        alt: 'Brane Logo',
-        src: 'img/branebrain.png',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'docsSidebar',
-          position: 'left',
-          label: 'Documentation',
-        },
-        {
-          href: 'https://github.com/wirewhiz/branescript',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Documentation',
-              to: '/docs/intro',
+    themes: ['@docusaurus/theme-mermaid'],
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'img/branebrain.png',
+        navbar: {
+            title: 'BraneScript',
+            logo: {
+                alt: 'Brane Logo',
+                src: 'img/branebrain.png',
             },
-          ],
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'docsSidebar',
+                    position: 'left',
+                    label: 'Documentation',
+                },
+                {
+                    href: 'https://github.com/wirewhiz/branescript',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
         },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Discord',
-              href: 'https://discord.gg/T3Td7PMDFX',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/thewirewhiz',
-            },
-          ],
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Documentation',
+                            to: '/docs/intro',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Discord',
+                            href: 'https://discord.gg/T3Td7PMDFX',
+                        },
+                        {
+                            label: 'Twitter',
+                            href: 'https://twitter.com/thewirewhiz',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/WireWhiz/BraneScript',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} WireWhiz. Built with Docusaurus.`,
         },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/WireWhiz/BraneScript',
-            },
-          ],
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
         },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} WireWhiz. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
